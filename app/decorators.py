c6b3f5b9205b0466660e61dc513f76383a6ac1d6
侯伟轩 <wxhou@yunjinginc.com>
2021-09-10 09:29:13 +0800

    update

--- app/decorators.py
+++ app/decorators.py
@@ -2,6 +2,7 @@ from functools import wraps
 from flask import request, current_app
 from common.response import ErrCode, response_err
 from books.model import User
+from flask_babel import _
 
 
 def dc_login_required(func):
@@ -10,12 +11,12 @@ def dc_login_required(func):
     def wrappers(*args, **kwargs):
         authorization = request.headers.get('Authorization')
         if authorization is None:
-            return response_err(ErrCode.COMMON_TOKEN_ERROR, 'token error')
+            return response_err(ErrCode.COMMON_TOKEN_ERROR, _('Not logged in'))
         token_type, token = authorization.split(None, 1)
         if token is None or token_type.lower() != 'bearer':
-            return response_err(ErrCode.COMMON_TOKEN_ERROR, 'token error')
+            return response_err(ErrCode.COMMON_TOKEN_ERROR, _('Token type error'))
         if User.validate_token(token):
             return func(*args, **kwargs)
-        return response_err(ErrCode.COMMON_TOKEN_ERROR, 'token error')
+        return response_err(ErrCode.COMMON_TOKEN_ERROR, _('Token validate error'))
 
     return wrappers
